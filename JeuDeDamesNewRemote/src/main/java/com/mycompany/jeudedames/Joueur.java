a7adf9b43b4be5b081ebcabf17b97fefb5da044a
Saleh eddine EL FDALI <elfdali31@gmail.com>
2023-01-11 15:09:12 +0100

    cleaning of code

--- JeuDeDamesNewRemote/src/main/java/com/mycompany/jeudedames/Joueur.java
+++ JeuDeDamesNewRemote/src/main/java/com/mycompany/jeudedames/Joueur.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.jeudedames;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 
-/**
- *
- * @author alexisibrahim
- */
+
 public class Joueur {
     
     private String username;
